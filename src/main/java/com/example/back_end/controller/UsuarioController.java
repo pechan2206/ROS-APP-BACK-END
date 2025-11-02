e50edd7b7b36e1960807792e61abc91805d9c745
Miguelardo <miguelhrodriguez48@gmail.com>
2025-11-01 20:11:52 -0500

    correccion de application.properties

---
+++ src/main/java/com/example/back_end/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package com.example.back_end.controller;
+
+
+import com.example.back_end.model.Usuario;
+import com.example.back_end.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "*")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listar(){
+        return usuarioService.listar();
+    }
+
+}
